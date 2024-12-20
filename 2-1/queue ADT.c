059057fd7fda8c1096bdfded21bc61ed794b77cc
Bhavit <84284964+bhavit069@users.noreply.github.com>
2024-12-20 19:10:05 +0530

    Update queue ADT.c

--- 2-1/queue ADT.c
+++ 2-1/queue ADT.c
@@ -1,1 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void enqueue(struct Node** head, int data) {
+    // insert at end
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    
+    temp->next = newNode;
+}
+
+void dequeue(struct Node** head) {
+    // delete from beginning
+    if (*head == NULL) {
+        printf("Stack is empty, can't pop");
+        return;
+    }
+    
+    struct Node* temp = *head;
+    *head = (*head)->next;
+    
+    free(temp);
+}
+
+void displayStack(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+
+int main() {
+    struct Node* head = NULL;
+    enqueue(&head, 5);
+    enqueue(&head, 15);
+    enqueue(&head, 10);
+    displayStack(head);
+    dequeue(&head);
+    displayStack(head);
+    return 0;
+}
